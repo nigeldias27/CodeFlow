634ae173458b5cbdfdfec9b6c6bdb62ebc2250a5
nigeldias27 <nigeldias27@yahoo.com>
2024-02-11 19:46:06 +0530

    Initial design

---
+++ processingscript.js
@@ -0,0 +1,8 @@
+function setup() {
+  let cnv = createCanvas(displayWidth, displayHeight);
+  cnv.parent("wordle_canvas");
+  background(255, 255, 255);
+}
+function draw() {
+  background(255, 255, 255);
+}
